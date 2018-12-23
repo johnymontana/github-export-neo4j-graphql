7b695883509259265ec1d5fc21d4e8c251a83844
William Lyon <lyonwj@gmail.com>
2018-12-23 16:57:54 -0700

    Add typedefs and neo4j-graphql.js

--- api/index.js
+++ api/index.js
@@ -1,29 +1,62 @@
 import { ApolloServer, gql } from "apollo-server";
 import { makeAugmentedSchema } from "neo4j-graphql-js";
+import { v1 as neo4j } from "neo4j-driver";
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
+const typeDefs = `
+  type Repository {
+    url: ID!
+    created_at: DateTime
+    description: String
+    name: String
+    website: String
+    webhooks: [Webhook] @relation(name: "HAS_WEBHOOK", direction: "OUT")
+    pull_requests: [PullRequest] @relation(name: "BASE", direction: "IN")
+    pr_count: Int @cypher(statement: "RETURN SIZE((this)<-[:BASE]-())")
   }
 
-  type Launch {
-    id: ID
+  type PullRequest {
+    url: ID!
+    title: String
+    created_at: DateTime
+    body: String
+    head: [Repository] @relation(name: "HEAD", direction: "IN")
+    base: [Repository] @relation(name: "BASE", direction: "IN")
+    user: User @relation(name: "OPENS", direction: "IN")
+  }
+
+  type User {
+    url: ID!
+    created_at: DateTime
+    company: String
+    location: String
     name: String
-    missionPatch: String
+    login: String
+    bio: String
+    repositories: [Repository] @relation(name: "OWNS", direction: "OUT")
+    pull_requests: [PullRequest] @relation(name: "OPENS", direction: "OUT")
+  }
+
+  type Webhook {
+    url: ID!
   }
 `;
 
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: (root, args, context) => "Hello world!"
-  }
-};
+const schema = makeAugmentedSchema({
+  typeDefs
+});
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI || "bolt://localhost:7687",
+  neo4j.auth.basic(
+    process.env.NEO4J_USER || "neo4j",
+    process.env.NEO4J_PASSWORD || "neo4j"
+  )
+);
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+  schema,
+  context: { driver }
 });
 
 server.listen().then(({ url }) => {
